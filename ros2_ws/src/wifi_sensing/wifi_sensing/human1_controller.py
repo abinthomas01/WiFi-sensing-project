b5f141cda2ebaf9355722c6e06e801d1dbd485ac
abinthomas01 <abint79024@gmail.com>
2026-09-07 16:03:29 +0530

    Update WiFi sensing project

--- ros2_ws/src/wifi_sensing/wifi_sensing/human1_controller.py
+++ ros2_ws/src/wifi_sensing/wifi_sensing/human1_controller.py
@@ -1,35 +1,64 @@
 import rclpy
 from rclpy.node import Node
 from gazebo_msgs.srv import SetEntityState
-from wifi_sensing.waypoints import WAYPOINTS
 import math
 
-class HumanSlider(Node):
-    def __init__(self):
-        super().__init__('human_slider')
-        self.client = self.create_client(SetEntityState, '/gazebo/set_entity_state')
+# 1. Define the routes directly in the code so it's easy to see
+STRANGLER_ROUTE = [
+    [4.98137, -0.005603], # Room 1
+    [4.80501, 1.95532],   # Room 2
+    [-2.01522, -2.00758]  # Room 3
+]
+
+WOMAN_ROUTE = [
+    [-1.69931, 2.28462],  # Kitchen
+    [1.67597, 1.00138],   # Hall
+    [-2.44060, -0.072066] # Bathroom
+]
+
+MAN_ROUTE = [
+    [-2.01522, -2.00758], # Room 3
+    [-1.69931, 2.28462],  # Kitchen
+    [4.98137, -0.005603]  # Room 1
+]
+
+# Girl: Bathroom -> Room 2 -> Kitchen
+GIRL_ROUTE = [
+    [-2.44060, -0.072066], # Bathroom
+    [4.80501, 1.95532],    # Room 2
+    [-1.69931, 2.28462]    # Kitchen
+]
+
+# Balthazar: Room 1 -> Hall -> Kitchen
+BALTHAZAR_ROUTE = [
+    [4.98137, -0.005603],  # Room 1
+    [1.67597, 1.00138],    # Hall
+    [-1.69931, 2.28462]    # Kitchen
+]
+
+# 2. This is the exact same sliding logic we used before
+class SliderBrain:
+    def __init__(self, node, model_name, waypoints):
+        self.model_name = model_name
+        self.waypoints = waypoints
+        self.client = node.create_client(SetEntityState, '/gazebo/set_entity_state')
         
-        while not self.client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Waiting for Gazebo /set_entity_state service...')
-        
-        self.model_name = "the_strangler"
         self.current_index = 0
-        
-        self.current_x = float(WAYPOINTS[0][0])
-        self.current_y = float(WAYPOINTS[0][1])
+        self.current_x = float(self.waypoints[0][0])
+        self.current_y = float(self.waypoints[0][1])
         self.speed = 0.05 
         
-        self.timer = self.create_timer(0.1, self.move_smoothly)
-        self.get_logger().info('Strangler Slider Started!')
+        # Create a timer for this specific model
+        self.timer = node.create_timer(0.1, self.move_smoothly)
+        node.get_logger().info(f'{self.model_name} started moving!')
 
     def move_smoothly(self):
-        if self.current_index >= len(WAYPOINTS) - 1:
-            self.get_logger().info('Reached the end of the route! Stopping.')
+        if self.current_index >= len(self.waypoints) - 1:
             self.timer.cancel()
             return
 
-        target_x = float(WAYPOINTS[self.current_index + 1][0])
-        target_y = float(WAYPOINTS[self.current_index + 1][1])
+        target_x = float(self.waypoints[self.current_index + 1][0])
+        target_y = float(self.waypoints[self.current_index + 1][1])
 
         dx = target_x - self.current_x
         dy = target_y - self.current_y
@@ -39,7 +68,6 @@ class HumanSlider(Node):
             self.current_x = target_x
             self.current_y = target_y
             self.current_index += 1
-            self.get_logger().info(f'Reached Waypoint {self.current_index + 1}')
         else:
             self.current_x += (dx / distance) * self.speed
             self.current_y += (dy / distance) * self.speed
@@ -56,9 +84,37 @@ class HumanSlider(Node):
 
         self.client.call_async(req)
 
+# 3. The main node that holds both brains
+class DualHumanSlider(Node):
+    def __init__(self):
+        super().__init__('dual_human_slider')
+        
+        # Wait for Gazebo to be ready
+        self.client = self.create_client(SetEntityState, '/gazebo/set_entity_state')
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Waiting for Gazebo /set_entity_state service...')
+
+        # Create the Strangler brain
+        self.strangler = SliderBrain(self, "the_strangler", STRANGLER_ROUTE)
+        
+        # Create the Woman brain
+        self.woman = SliderBrain(self, "realistic_woman_walking_animated_1", WOMAN_ROUTE)
+
+        # Create the Man brain
+        self.man = SliderBrain(self, "middle_age_man__rigged__animated__free", MAN_ROUTE)
+
+        # Create the Girl brain
+        self.girl = SliderBrain(self, "girl_with_clothes", GIRL_ROUTE)
+
+        # Create the Blathazar brain
+        self.balthazar = SliderBrain(self, "balthazar_rigged_animated", BALTHAZAR_ROUTE)
+
+
+
+
 def main(args=None):
     rclpy.init(args=args)
-    node = HumanSlider()
+    node = DualHumanSlider()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
